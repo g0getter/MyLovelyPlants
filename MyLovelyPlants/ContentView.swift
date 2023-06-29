9979807e07356acb0cefb1e4e19abcf55050c286
Aiden <nagyeong.yeo1023@gmail.com>
2023-06-29 22:52:47 +0900

    Create a project

---
+++ MyLovelyPlants/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyLovelyPlants
+//
+//  Created by 여나경 on 2023/06/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyLovelyPlants/MyLovelyPlantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLovelyPlantsApp.swift
+//  MyLovelyPlants
+//
+//  Created by 여나경 on 2023/06/29.
+//
+
+import SwiftUI
+
+@main
+struct MyLovelyPlantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
